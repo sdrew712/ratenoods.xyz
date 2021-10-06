2be8ed9e768fcb4ec749d4ace71c9ae21529e78e
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-06 14:54:10 -0700

    edit manifest.json

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import Headroom from "react-headroom";
 import { Link } from "react-router-dom";
+import "./top-bar.css";
 
 const TopBar = () => {
   return (
     <Headroom>
-      <div>
-        <Link to="/">Rate Noods</Link>
-      </div>
+      <div className="top-bar">
+        <div>
+          <Link to="/" id="site-title">
+            Rate Noods
+          </Link>
+        </div>
 
-      <div>
-        <Link to="/upgrades">Upgrades</Link>
-        <Link to="/about">About</Link>
+        <div>
+          <Link to="/upgrades" className="nav-link">
+            Upgrades
+          </Link>
+          <Link to="/about" className="nav-link">
+            About
+          </Link>
+        </div>
       </div>
     </Headroom>
   );
