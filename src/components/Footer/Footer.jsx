2aa9e87f8b6e54460cc48055792f5571af88ef81
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-06 15:49:17 -0700

    add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import "./footer.css";
+import linkedInLogo from "./linkedin-logo.png";
+import gitHubLogo from "./github-logo-64px.png";
 
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <div id="footer">
+      <div>Made with ♥ by Sarah Drew</div>
+      <div id="icon-container">
+        <img src={linkedInLogo} alt="linkedin logo" id="linkedin-logo" />
+        <img src={gitHubLogo} alt="github logo" id="github-logo" />
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
