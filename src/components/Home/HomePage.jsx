b29d546dee7b61f7fa24597cfd9c8a47d3735c70
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-06 15:31:31 -0700

    add header image

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import "./home-page.css";
 
 const HomePage = () => {
-  return <>home page!</>;
+  return (
+    <>
+      <div id="header-img">
+        <div id="header-text">
+          Your one stop for everything instant noodles.
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
