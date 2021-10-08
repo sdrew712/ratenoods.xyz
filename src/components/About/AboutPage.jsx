6f60574d4b20b60a5958333987c4a899ccb050f2
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-07 20:32:53 -0700

    update aboutpage text

--- src/components/About/AboutPage.jsx
+++ src/components/About/AboutPage.jsx
@@ -6,18 +6,22 @@ const AboutPage = () => {
   return (
     <>
       <PageHeader headerText="About me!" />
-      <div className="page-title-container">
-        <h1 className="page-title">I’m Sarah</h1>
-        <hr />
-      </div>
 
       <div id="about-text-container">
+        <div className="page-title-container">
+          <h1 className="page-title">Who am I?</h1>
+          <hr />
+        </div>
         <p className="about-text">
-          A web developer and current DevMountain student living in Tempe, AZ. I
-          spend my days learning how to make this site even better and trying
-          different instant noodle flavors.
+          I'm Sarah, a web developer and current DevMountain student living in
+          Tempe, AZ. I spend my days learning how to make this site even better
+          and trying different instant noodle flavors.
         </p>
         <br />
+        <div className="page-title-container">
+          <h1 className="page-title">Get in touch</h1>
+          <hr />
+        </div>
         <p className="about-text">
           You can check out my source code here and connect with me on LinkedIn
           here.
