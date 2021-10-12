a9f596e994cfaccf1c319a3f846ef68427828df3
Sarah Drew <drew.sarah.e@gmail.com>
2021-10-12 14:53:14 -0700

    update card alt text

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -4,7 +4,7 @@ import "./card.css";
 const Card = ({ name, rating, bodyText, image }) => {
   return (
     <div className="card">
-      <img src={image} alt="noodle_img" className="card-img" />
+      <img src={image} alt="Instant noodle package" className="card-img" />
       <div className="card-text-container">
         <h2 className="card-name">{name}</h2>
         <p className="rating">{rating}</p>
